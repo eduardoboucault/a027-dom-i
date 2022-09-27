93e5cf9a0d756c791075a4946cd86cffae8e498e
Eduardo Boucault <boucault.labenu@outlook.com>
2022-09-27 17:55:57 -0300

    esperando feedback

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const fruta1 = document.getElementById("fruta-1")
+fruta1.innerHTML = frutas[0]
+
+const fruta2 = document.getElementById("fruta-2")
+fruta2.innerHTML = frutas[1]
+
+const fruta3 = document.getElementById("fruta-3")
+fruta3.innerHTML = frutas[2]
+
+const fruta4 = document.getElementById("fruta-4")
+
+const input = document.getElementById("fruta-digitada")
+
+function adicionaFruta(){
+    fruta4.innerHTML = input.value
+    input.value = ""
+}
